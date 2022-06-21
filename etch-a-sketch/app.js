c80e0a330a5f19079d2b757d5e9273293b7bbe6b
kevinkimcd <kevinkimcd@gmail.com>
2022-06-21 16:41:24 -0400

    hover coloration functional

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,7 +1,15 @@
 const canvas = document.querySelector('.container');
 
-for (i = 0; i < 16; i++) {
+for (i = 0; i < 256; i++) {
   let div = document.createElement('div');
   div.setAttribute('class', 'grid-square');
   canvas.appendChild(div);
 }
+
+const gridSquare = document.querySelectorAll('.grid-square');
+
+gridSquare.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.classList.add('colored');
+  });
+});
